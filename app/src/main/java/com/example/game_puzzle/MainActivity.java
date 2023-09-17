6714619e405e3716ef8bac985e457d57c50a6938
WaannAlter <134024741+WaannAlter@users.noreply.github.com>
2023-09-17 18:44:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/game_puzzle/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.game_puzzle;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GridLayout gridLayout;
+    private List<Button> buttons;
+    private Button emptyBlock;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gridLayout = findViewById(R.id.gridLayout);
+        emptyBlock = findViewById(R.id.emptyBlock);
+
+        buttons = new ArrayList<>();
+        for (int i = 1; i <= 15; i++) {
+            int resID = getResources().getIdentifier("block" + i, "id", getPackageName());
+            Button block = findViewById(resID);
+            buttons.add(block);
+            block.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onBlockClick(block);
+                }
+            });
+        }
+
+        // Set listener untuk tombol Acak
+        Button buttonRandomize = findViewById(R.id.buttonRandomize);
+        buttonRandomize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shuffleBlocks();
+            }
+        });
+    }
+
+    // Implementasi logika ketika tombol blok di klik di sini
+    private void onBlockClick(Button block) {
+        // Temukan indeks blok yang diklik dalam daftar tombol
+        int clickedIndex = buttons.indexOf(block);
+
+        // Temukan indeks blok kosong dalam daftar tombol
+        int emptyIndex = buttons.indexOf(emptyBlock);
+
+        // Periksa apakah blok yang diklik berada di sebelah blok kosong
+        if (isAdjacent(clickedIndex, emptyIndex)) {
+            // Tukar teks dan tombol antara blok yang diklik dan blok kosong
+            CharSequence clickedText = block.getText();
+            block.setText(emptyBlock.getText());
+            emptyBlock.setText(clickedText);
+
+            // Blok kosong sekarang menjadi blok yang diklik
+            emptyBlock = block;
+        }
+    }
+
+    // Implementasi logika untuk mengacak blok-blok
+    private void shuffleBlocks() {
+        Collections.shuffle(buttons);
+
+        // Perbarui tampilan grid dengan urutan blok yang sudah diacak
+        gridLayout.removeAllViews();
+        for (Button button : buttons) {
+            gridLayout.addView(button);
+        }
+    }
+
+    // Periksa apakah dua blok bersebelahan
+    private boolean isAdjacent(int index1, int index2) {
+        // Kode ini mengasumsikan grid ukuran 4x4, Anda dapat menyesuaikannya jika grid Anda berbeda
+        int gridSize = 4;
+        int row1 = index1 / gridSize;
+        int col1 = index1 % gridSize;
+        int row2 = index2 / gridSize;
+        int col2 = index2 % gridSize;
+
+        return (Math.abs(row1 - row2) == 1 && col1 == col2) || (Math.abs(col1 - col2) == 1 && row1 == row2);
+    }
+}
